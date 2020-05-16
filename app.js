d77bfae7e04174d28f2e6864600c083c2daff68a
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-05-16 11:38:00 -0500

    cors added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose
   .connect(process.env.DB, {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require("../models/User/user");
+const User = require("../models/User");
 
 /* GET users */
 router.get("/", (req, res) => {
-  res.json({ message: "todo funciona chido" })
+  res.json({ message: "todo funciona chido" });
 });
 
 router.post("/", (req, res) => {
@@ -12,7 +12,7 @@ router.post("/", (req, res) => {
     .then((user) => {
       res.status(201).json({ result: user });
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
