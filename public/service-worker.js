999cb2ef6b6dad6bed19a316fa24ddddab0be10b
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-05-19 20:43:51 -0500

    prueba deploy 4

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f93769c39150ed1b4f1dd1d6bea03453.js"
+  "/precache-manifest.7967137b04121804efb07e09b2d28539.js"
 );
 
 self.addEventListener('message', (event) => {
